e7a3b4ed011ef42042e1caa5cfa383516e24f607
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2023-12-28 14:35:36 -0500

    Merge accept current changes for Exceptions

--- domino_hidden_patterns/game/exceptions/gameExceptions.py
+++ domino_hidden_patterns/game/exceptions/gameExceptions.py
@@ -2,7 +2,6 @@ class DeckEmptyException(Exception):
     """The Deck is empty and cannot be drawn from."""
     
     def __init__(self, message='The Deck is empty and cannot be drawn from.'):
-<<<<<<< HEAD
         super(DeckEmptyException, self).__init__(message)
 
 
@@ -18,6 +17,3 @@ class NoCompatibleTilesException(Exception):
     
     def __init__(self, message='There are no pips on the Snake endpoints that match those of the new Tile.'):
         super(DeckEmptyException, self).__init__(message)
-=======
-        super(DeckEmptyException, self).__init__(message)
->>>>>>> b9088d2f784a13f56dde60594e2b031814bbb9ee
